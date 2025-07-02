58d35cef5f8a660c20be1c056b3a10febcf48a4f
Melania Rizzuto <melrizzuto4@gmail.com>
2025-07-02 19:12:35 +0200

    feat: created ProductCard component

---
+++ components/ProductCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <li class="product-card">
+    <NuxtLink :to="`/products/${product.id}`">
+      <h2>{{ product.name }}</h2>
+    </NuxtLink>
+    <p>{{ product.description }}</p>
+    <p>
+      <strong>{{ product.price }} €</strong>
+    </p>
+  </li>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  product: {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+  };
+}>();
+</script>
+
+<style scoped>
+.product-card {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  border-radius: 8px;
+  margin-bottom: 1rem;
+}
+</style>
