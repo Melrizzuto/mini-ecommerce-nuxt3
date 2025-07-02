1e811c2cc88452b5db24650aaf7ee4e4fddd77b1
Melania Rizzuto <melrizzuto4@gmail.com>
2025-07-02 19:06:02 +0200

    fix: changed posts in products.ts feat:added a products.vue

---
+++ pages/products.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Prodotti</h1>
+    <ul v-if="products">
+      <li v-for="product in products" :key="product.id">
+        <h2>{{ product.name }}</h2>
+        <p>{{ product.description }}</p>
+        <p>
+          <strong>{{ product.price }} €</strong>
+        </p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+}
+
+const { data: products } = await useFetch<Product[]>("/api/products");
+</script>
